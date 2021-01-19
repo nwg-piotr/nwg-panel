3e0eafb3171980d9b1a166b9ca1beb0fb4a3941d
piotr <nwg.piotr@gmail.com>
2021-01-19 22:43:26 +0100

    threading looks better

--- nwg_panel/modules/executor.py
+++ nwg_panel/modules/executor.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
 
-from gi.repository import Gtk, GLib, GdkPixbuf
+from gi.repository import GLib
 
-import sys
-sys.path.append('../')
-
-import os
 import subprocess
-from threading import Thread
-from queue import Queue, Empty
+
+import threading
 
 
-
-import nwg_panel.common
 from nwg_panel.tools import check_key
 
 import gi
@@ -30,11 +24,16 @@ class Executor(Gtk.EventBox):
         self.add(self.box)
         self.image = Gtk.Image.new_from_icon_name("wtf", Gtk.IconSize.MENU)
         self.label = Gtk.Label("")
-        # We won't warn about this key missing, so it needs to be mentioned id README
+
+        # We won't warn about these keys missing, so they need to be mentioned id README
         if "css-name" in settings:
             self.label.set_property("name", settings["css-name"])
         else:
             self.label.set_property("name", "executor-label")
+        
+        if "icon-size" not in self.settings:
+            self.settings["icon-size"] = 16
+        
         self.icon_path = None
         
         self.build_box()
@@ -43,55 +42,45 @@ class Executor(Gtk.EventBox):
         check_key(settings, "interval", 0)
         if settings["interval"] > 0:
             Gdk.threads_add_timeout_seconds(GLib.PRIORITY_LOW, settings["interval"], self.refresh)
-            #GLib.timeout_add(settings["interval"], self.refresh)
 
-    def enqueue_output(self, out, queue):
-        for line in iter(out.readline, b''):
-            queue.put(line)
-        out.close()
-
-        # Depending on the executor, we expect 1 or 2 lines
-        lines = []
-        for i in range(2):
-            try:
-                line = queue.get_nowait()  # or q.get(timeout=.1)
-                lines.append(line.decode("utf-8").strip())
-            except Empty:
-                pass
-            
-        if len(lines) > 1:
-            if self.icon_path != lines[0]:  # update image only if changed
-
-                pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(lines[0], 16, 16)
-                self.image.set_from_pixbuf(pixbuf)
-                if not self.image.get_visible():
-                    self.image.show()
-
-                self.icon_path = lines[0]
-
-            self.label.set_text(lines[1])
-        elif len(lines) == 1:
+    def update_widget(self, output):
+        if len(output) == 1:
             if self.image.get_visible():
                 self.image.hide()
+            self.label.set_text(output[0].strip())
+        elif len(output) == 2:
+            new_path = output[0].strip()
+            if new_path != self.icon_path:
+                try:
+                    pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(
+                        new_path, self.settings["icon-size"], self.settings["icon-size"])
+                    self.image.set_from_pixbuf(pixbuf)
+                    self.icon_path = new_path
+                except:
+                    print("Failed setting image from {}".format(output[0].strip()))
+                if not self.image.get_visible():
+                    self.image.show()
+            
+            self.label.set_text(output[1].strip())
+        
+        return False
 
-            if self.label.get_text() != lines[0]:
-                self.label.set_text(lines[0])
-                
-        else:
-            print("Couldnt refresh executor")
+    def get_output(self):
+        if "script" in self.settings:
+            try:
+                output = subprocess.check_output(self.settings["script"].split()).decode("utf-8").splitlines()
+                GLib.idle_add(self.update_widget, output)
+            except Exception as e:
+                print(e)
+        
+    def refresh(self):
+        thread = threading.Thread(target=self.get_output)
+        thread.daemon = True
+        thread.start()
+        return True
 
     def build_box(self):
         self.box.pack_start(self.image, False, False, 4)
         self.box.pack_start(self.label, False, False, 0)
         self.label.show()
-    
-    def refresh(self):
-        if "script" in self.settings:
-            ON_POSIX = 'posix' in sys.builtin_module_names
-            p = subprocess.Popen(self.settings["script"].split(), stdout=subprocess.PIPE, close_fds=ON_POSIX)
-            q = Queue()
-            t = Thread(target=self.enqueue_output, args=(p.stdout, q))
-            t.daemon = True  # thread dies with the program
-            t.start()
-            
-            return True
+
