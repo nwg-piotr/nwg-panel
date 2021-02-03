f8582848c138416206603f26e6ec776c5c90ff5d
Piotr Miller <nwg.piotr@gmail.com>
2021-02-03 14:59:02 +0100

    Reading data

--- nwg_panel/config.py
+++ nwg_panel/config.py
@@ -2,7 +2,6 @@
 
 import os
 import sys
-import json
 
 import gi
 
@@ -13,6 +12,8 @@ from nwg_panel.tools import get_config_dir, load_json, list_outputs, check_key, 
 
 config_dir = get_config_dir()
 configs = {}
+editor = None
+outputs = list_outputs()
 
 
 def handle_keyboard(window, event):
@@ -100,10 +101,94 @@ class PanelSelector(Gtk.Window):
         self.show_all()
     
     def on_button_clicked(self, button, file, panel):
-        print("Panel {} in {}".format(panel, file))
+        editor.set_values(file, panel)
+        self.close()
+        
         
     def release_parent(self, w, parent):
         parent.set_sensitive(True)
+
+
+class PanelEditor(object):
+    def __init__(self):
+        self.config = None
+        self.panel = {}
+        builder = Gtk.Builder()
+        builder.add_from_file("glade/panel_edit.glade")
+        self.lb_panel_desc = builder.get_object("label-desc")
+        self.eb_name = builder.get_object("name")
+
+        self.cb_output = builder.get_object("output")
+        for key in outputs:
+            self.cb_output.append(key, key)
+        
+        self.cb_position = builder.get_object("position")
+        self.cb_layer = builder.get_object("layer")
+        
+        self.eb_width = builder.get_object("width")
+        self.eb_height = builder.get_object("height")
+        self.eb_margin_top = builder.get_object("margin-top")
+        self.eb_margin_bottom = builder.get_object("margin-bottom")
+        self.eb_padding_horizontal = builder.get_object("padding-horizontal")
+        self.eb_padding_vertical = builder.get_object("padding-vertical")
+        self.eb_spacing = builder.get_object("spacing")
+        self.eb_items_padding = builder.get_object("items-padding")
+
+        self.cb_icons = builder.get_object("icons")
+
+        self.eb_css_name = builder.get_object("css-name")
+        
+        self.window = builder.get_object("panel_edit_win")
+        self.window.connect('destroy', Gtk.main_quit)
+        self.window.connect("key-release-event", handle_keyboard)
+        self.window.set_sensitive(False)
+
+        self.window.show_all()
+        
+    def set_values(self, file, panel_num):
+        print(file, panel_num)
+        self.config = load_json(file)
+        self.panel = self.config[panel_num]
+        check_key(self.panel, "name", "")
+        check_key(self.panel, "output", "")
+        check_key(self.panel, "position", "")
+        
+        check_key(self.panel, "width", "auto")
+        check_key(self.panel, "height", 0)
+        check_key(self.panel, "margin-top", 0)
+        check_key(self.panel, "margin-bottom", 0)
+        check_key(self.panel, "padding-horizontal", 0)
+        check_key(self.panel, "padding-vertical", 0)
+        check_key(self.panel, "spacing", 0)
+        check_key(self.panel, "items-padding", 0)
+        check_key(self.panel, "icons", "")
+        check_key(self.panel, "css-name", "")
+        
+        #self.lb_panel_desc.set_text("Panel #{} in {}".format(panel_num, file))
+        self.lb_panel_desc.set_text("Panel #{} in {}".format(panel_num, file))
+        self.eb_name.set_text(self.panel["name"])
+
+        self.cb_position.set_active_id(self.panel["position"])
+        self.cb_layer.set_active_id(self.panel["layer"])
+        
+        self.eb_width.set_text(str(self.panel["width"]))
+        self.eb_height.set_text(str(self.panel["height"]))
+        self.eb_margin_top.set_text(str(self.panel["margin-top"]))
+        self.eb_margin_bottom.set_text(str(self.panel["margin-bottom"]))
+        self.eb_padding_horizontal.set_text(str(self.panel["padding-horizontal"]))
+        self.eb_padding_vertical.set_text(str(self.panel["padding-vertical"]))
+        self.eb_spacing.set_text(str(self.panel["spacing"]))
+        self.eb_items_padding.set_text(str(self.panel["items-padding"]))
+
+        if self.panel["icons"]:
+            self.cb_icons.set_active_id(self.panel["icons"])
+        else:
+            self.cb_icons.set_active_id("gtk")
+            
+        if self.panel["output"] and self.panel["output"] in outputs:
+            self.cb_output.set_active_id(self.panel["output"])
+        
+        self.eb_css_name.set_text(str(self.panel["css-name"]))
 
 
 def main():
@@ -112,16 +197,9 @@ def main():
 
     GLib.set_prgname('nwg-panel-config')
 
-    builder = Gtk.Builder()
-    builder.add_from_file("glade/panel_edit.glade")
-    
-    window = builder.get_object("panel_edit_win")
-    window.show_all()
-    window.connect('destroy', Gtk.main_quit)
-    window.connect("key-release-event", handle_keyboard)
-    window.set_sensitive(False)
-
-    selector = PanelSelector(window)
+    global editor
+    editor = PanelEditor()
+    selector = PanelSelector(editor.window)
 
     Gtk.main()
 
