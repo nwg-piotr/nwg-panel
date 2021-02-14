658663953f2f96c9aae015d2a6942c9fa26afc33
piotr <nwg.piotr@gmail.com>
2021-02-14 23:54:23 +0100

    uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rm -r /usr/lib/python3.9/site-packages/nwg-panel*
+rm -r /usr/lib/python3.9/site-packages/nwg_panel*
 rm /usr/bin/nwg-panel
 rm /usr/bin/nwg-panel-config
 rm  /usr/share/pixmaps/nwg-panel.svg
