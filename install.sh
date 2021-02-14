29f49fdeb4dd600e0589b755916f1f50aab7e936
piotr <nwg.piotr@gmail.com>
2021-02-14 23:49:37 +0100

    uninstall script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo python setup.py install --optimize=1
+python setup.py install --optimize=1
 cp nwg-panel.svg /usr/share/pixmaps/
 cp nwg-panel-config.desktop /usr/share/applications/
