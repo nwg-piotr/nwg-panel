b4103aeb77580bdb5fd644f2d11a24bb7371b3a5
piotr <nwg.piotr@gmail.com>
2021-07-29 01:32:41 +0200

    fix uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -4,4 +4,5 @@ rm -r /usr/lib/python3.9/site-packages/nwg_panel*
 rm /usr/bin/nwg-panel
 rm /usr/bin/nwg-panel-config
 rm  /usr/share/pixmaps/nwg-panel.svg
+rm  /usr/share/pixmaps/nwg-shell.svg
 rm  /usr/share/applications/nwg-panel-config.desktop
