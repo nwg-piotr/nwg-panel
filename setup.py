eb1877e82998a598950934d15c00bdcb5c25bb07
piotr <nwg.piotr@gmail.com>
2022-02-22 00:05:05 +0100

    bump to 0.6.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(f_name):
 
 setup(
     name='nwg-panel',
-    version='0.6.1',
+    version='0.6.2',
     description='GTK3-based panel for sway window manager',
     packages=find_packages(),
     include_package_data=True,
