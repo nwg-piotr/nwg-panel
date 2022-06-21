d0ea9b5e561bc3848771c76e56738e091d61fa83
az <102905929+az01101001@users.noreply.github.com>
2022-06-21 19:00:47 +0800

    changed popup window to use GtkLayerShell

--- nwg_panel/modules/brightness_slider.py
+++ nwg_panel/modules/brightness_slider.py
@@ -26,13 +26,17 @@ class BrightnessSlider(Gtk.EventBox):
             "root-css-name": "brightness-module",
             "css-name": "brightness-popup",
             "angle": 0.0,
-            "popup-length": 256,
             "icon-placement": "start",
             "backlight-device": "",
             "slider-orientation": "vertical",
             "slider-inverted": True,
             "popup-icon-placement": "off",
-            "popup-position": "off",
+            "popup-horizontal-alignment": "left",
+            "popup-vertical-alignment": "center",
+            "popup-width": 256,
+            "popup-height": 64,
+            "popup-horizontal-margin": 100,
+            "popup-vertical-margin": 100,
             "step-size": 2,
         }
         for key in defaults:
@@ -110,7 +114,6 @@ class BrightnessSlider(Gtk.EventBox):
     
     def on_button_press(self, w, event):
         if not self.popup_window.get_visible():
-            self.popup_window.update_position()
             self.popup_window.show_all()
         else:
             self.popup_window.hide()
@@ -132,7 +135,6 @@ class BrightnessSlider(Gtk.EventBox):
     def on_enter_notify_event(self, widget, event):
         if self.settings["hover-opens"]:
             if not self.popup_window.get_visible():
-                self.popup_window.update_position()
                 self.popup_window.show_all()
         else:
             widget.set_state_flags(Gtk.StateFlags.DROP_ACTIVE, clear=False)
@@ -147,19 +149,13 @@ class BrightnessSlider(Gtk.EventBox):
         widget.unset_state_flags(Gtk.StateFlags.DROP_ACTIVE)
         widget.unset_state_flags(Gtk.StateFlags.SELECTED)
         return True
-        
-    def get_panel_window(self):
-        parent = self
-        while True:
-            parent = parent.get_parent()
-            if type(parent) == Gtk.Window:
-                return parent
-            elif parent == None:
-                return None
 
 class PopupWindow(Gtk.Window):
-    def __init__(self, parent, settings, icons_path=""):
-        Gtk.Window.__init__(self, type_hint=Gdk.WindowTypeHint.DOCK, type=Gtk.WindowType.POPUP)
+    def __init__(self, parent, settings, monitor=None, icons_path=""):
+        Gtk.Window.__init__(self, type_hint=Gdk.WindowTypeHint.NORMAL)
+        GtkLayerShell.init_for_window(self)
+        if monitor:
+            GtkLayerShell.set_monitor(self, monitor)
 
         self.parent = parent
         self.settings = settings
@@ -180,21 +176,35 @@ class PopupWindow(Gtk.Window):
         self.box = Gtk.Box(spacing=0)
         if self.settings["slider-orientation"] == "vertical":
             self.box.set_orientation(Gtk.Orientation.VERTICAL)
+            self.bri_scale = Gtk.Scale.new_with_range(orientation=Gtk.Orientation.VERTICAL, min=0, max=100, step=1)
         elif self.settings["slider-orientation"] == "horizontal":
             self.box.set_orientation(Gtk.Orientation.HORIZONTAL)
+            self.bri_scale = Gtk.Scale.new_with_range(orientation=Gtk.Orientation.HORIZONTAL, min=0, max=100, step=1)
+
+        self.bri_scale.set_inverted(self.settings["slider-inverted"])
+        self.bri_scale_handler = self.bri_scale.connect("value-changed", self.set_bri)
+
+        self.bri_icon_name = "view-refresh-symbolic"
+        self.bri_image = Gtk.Image.new_from_icon_name(self.bri_icon_name, Gtk.IconSize.MENU)
             
         eb.add(self.box)
         self.add(eb)
 
-        self.bri_icon_name = "view-refresh-symbolic"
-        self.bri_image = Gtk.Image.new_from_icon_name(self.bri_icon_name, Gtk.IconSize.MENU)
+        if settings["popup-vertical-alignment"] == "top":
+            GtkLayerShell.set_anchor(self, GtkLayerShell.Edge.TOP, True)
+            GtkLayerShell.set_margin(self, GtkLayerShell.Edge.TOP, settings["popup-vertical-margin"])
+        elif settings["popup-vertical-alignment"] == "bottom":
+            GtkLayerShell.set_anchor(self, GtkLayerShell.Edge.BOTTOM, True)
+            GtkLayerShell.set_margin(self, GtkLayerShell.Edge.BOTTOM, settings["popup-vertical-margin"])
 
-        if self.settings["slider-orientation"] == "vertical":
-            self.bri_scale = Gtk.Scale.new_with_range(orientation=Gtk.Orientation.VERTICAL, min=0, max=100, step=1)
-        elif self.settings["slider-orientation"] == "horizontal":
-            self.bri_scale = Gtk.Scale.new_with_range(orientation=Gtk.Orientation.HORIZONTAL, min=0, max=100, step=1)
-        self.bri_scale.set_inverted(self.settings["slider-inverted"])
-        self.bri_scale_handler = self.bri_scale.connect("value-changed", self.set_bri)
+        if settings["popup-horizontal-alignment"] == "left":
+            GtkLayerShell.set_anchor(self, GtkLayerShell.Edge.LEFT, True)
+            GtkLayerShell.set_margin(self, GtkLayerShell.Edge.LEFT, settings["popup-horizontal-margin"])
+        elif settings["popup-horizontal-alignment"] == "right":
+            GtkLayerShell.set_anchor(self, GtkLayerShell.Edge.RIGHT, True)
+            GtkLayerShell.set_margin(self, GtkLayerShell.Edge.RIGHT, settings["popup-horizontal-margin"])
+
+        Gtk.Widget.set_size_request(self.box, settings["popup-width"], settings["popup-height"])
 
         self.build_box()
 
@@ -242,99 +252,6 @@ class PopupWindow(Gtk.Window):
     def set_bri(self, slider):
         self.parent.bri_value = int(slider.get_value())
         set_brightness(self.parent.bri_value, device=self.settings["backlight-device"])
-    
-    def update_position(self):
-        panel = self.get_transient_for()
-        if panel == None:
-            panel = self.parent.get_panel_window()
-            if panel:
-                self.set_transient_for(panel)
-        
-        if panel:
-            panel_width, panel_height = panel.get_size()
-            if self.settings["popup-position"] == "top":
-                button_start = self.parent.get_window().get_position().x
-                button_length = self.parent.get_allocated_width()
-                button_midpoint = button_start + (button_length / 2)
-
-                if self.settings["slider-orientation"] == "horizontal":
-                    popup_width = self.settings["popup-length"]
-                    popup_height = max(50, button_length)
-                elif self.settings["slider-orientation"] == "vertical":
-                    popup_width = max(50, button_length)
-                    popup_height = self.settings["popup-length"]
-                
-                x = button_midpoint - (popup_width / 2)
-                y = -popup_height
-
-                if x + popup_width > panel_width:
-                    x = panel_width - popup_width
-                
-                if x < 0:
-                    x = 0
-            elif self.settings["popup-position"] == "bottom":
-                button_start = self.parent.get_window().get_position().x
-                button_length = self.parent.get_allocated_width()
-                button_midpoint = button_start + (button_length / 2)
-
-                if self.settings["slider-orientation"] == "horizontal":
-                    popup_width = self.settings["popup-length"]
-                    popup_height = max(50, button_length)
-                elif self.settings["slider-orientation"] == "vertical":
-                    popup_width = max(50, button_length)
-                    popup_height = self.settings["popup-length"]
-                
-                x = button_midpoint - (popup_width / 2)
-                y = panel_height
-
-                if x + popup_width > panel_width:
-                    x = panel_width - popup_width
-                
-                if x < 0:
-                    x = 0
-            elif self.settings["popup-position"] == "left":
-                button_start = self.parent.get_window().get_position().y
-                button_length = self.parent.get_allocated_height()
-                button_midpoint = button_start + (button_length / 2)
-
-                if self.settings["slider-orientation"] == "horizontal":
-                    popup_width = self.settings["popup-length"]
-                    popup_height = max(50, button_length)
-                elif self.settings["slider-orientation"] == "vertical":
-                    popup_width = max(50, button_length)
-                    popup_height = self.settings["popup-length"]
-                
-                x = -popup_width
-                y = button_midpoint - (popup_height / 2)
-
-                if y + popup_height > panel_height:
-                    y = panel_height - popup_height
-                
-                if y < 0:
-                    y = 0
-            elif self.settings["popup-position"] == "right":
-                button_start = self.parent.get_window().get_position().y
-                button_length = self.parent.get_allocated_height()
-                button_midpoint = button_start + (button_length / 2)
-
-                if self.settings["slider-orientation"] == "horizontal":
-                    popup_width = self.settings["popup-length"]
-                    popup_height = max(50, button_length)
-                elif self.settings["slider-orientation"] == "vertical":
-                    popup_width = max(50, button_length)
-                    popup_height = self.settings["popup-length"]
-                
-                x = panel_width
-                y = button_midpoint - (popup_height / 2)
-
-                if y + popup_height > panel_height:
-                    y = panel_height - popup_height
-                
-                if y < 0:
-                    y = 0
-                
-            Gtk.Widget.set_size_request(self.box, popup_width, popup_height)
-            self.move(x, y)
 
 def bri_icon_name(value):
     icon_name = "display-brightness-low-symbolic"
