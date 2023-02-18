ede97b1c1f6af391290752e873891bc055458595
piotr <nwg.piotr@gmail.com>
2023-02-18 01:12:10 +0100

    copy nwg-processes icon

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 python3 setup.py install --optimize=1
 cp nwg-panel.svg /usr/share/pixmaps/
 cp nwg-shell.svg /usr/share/pixmaps/
+cp nwg-processes.svg /usr/share/pixmaps/
 cp nwg-panel-config.desktop /usr/share/applications/
