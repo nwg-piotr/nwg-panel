d9ff59dffd72250a026472c0458b6883c527a510
piotr <nwg.piotr@gmail.com>
2023-04-27 14:21:15 +0200

    clear unused stuff

--- nwg_panel/modules/hyprland_workspaces.py
+++ nwg_panel/modules/hyprland_workspaces.py
@@ -4,7 +4,7 @@ import json
 from gi.repository import Gtk, Gdk
 
 import nwg_panel.common
-from nwg_panel.tools import check_key, update_image, update_image_fallback_desktop, hyprctl
+from nwg_panel.tools import check_key, update_image_fallback_desktop, hyprctl
 
 
 class HyprlandWorkspaces(Gtk.Box):
@@ -12,12 +12,8 @@ class HyprlandWorkspaces(Gtk.Box):
         Gtk.Box.__init__(self, orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
         self.settings = settings
         self.num_box = Gtk.Box.new(Gtk.Orientation.HORIZONTAL, 0)
-        self.ws_num2box = {}
-        self.ws_num2lbl = {}
-        self.ws_id2name = {}  # new
+        self.ws_id2name = {}
         self.name_label = Gtk.Label()
-        self.win_id = ""
-        self.win_pid = None
         self.icon = Gtk.Image()
         self.layout_icon = Gtk.Image()
         self.icons_path = icons_path
@@ -47,42 +43,8 @@ class HyprlandWorkspaces(Gtk.Box):
 
         self.pack_start(self.num_box, False, False, 0)
 
-        # ws_num = -1
-        # if self.i3.get_tree().find_focused():
-        #     ws_num, win_name, win_id, non_empty, win_layout, numbers = self.find_details()
-        #
-        # if len(self.settings["custom-labels"]) == 1:
-        #     self.settings["custom-labels"] *= len(self.settings["numbers"])
-        # elif len(self.settings["custom-labels"]) != len(self.settings["numbers"]):
-        #     self.settings["custom-labels"] = []
-        #
-        # if len(self.settings["focused-labels"]) == 1:
-        #     self.settings["focused-labels"] *= len(self.settings["numbers"])
-        # elif len(self.settings["focused-labels"]) != len(self.settings["numbers"]):
-        #     self.settings["focused-labels"] = []
-
         for i in range(1, self.settings["num-ws"] + 1):
             self.ws_nums.append(i)
-
-        # for num in self.workspaces:
-        #     eb, lbl = self.build_number(num, str(num))
-        #     self.num_box.pack_start(eb, False, False, 0)
-
-        # for idx, num in enumerate(self.settings["numbers"]):
-        #     if num == str(ws_num) and self.settings["focused-labels"]:
-        #         label = self.settings["focused-labels"][idx]
-        #     elif self.settings["custom-labels"]:
-        #         label = self.settings["custom-labels"][idx]
-        #     else:
-        #         label = str(num)
-        #
-        #     eb, lbl = self.build_number(num, label)
-        #     self.num_box.pack_start(eb, False, False, 0)
-        #
-        #     if num == str(ws_num):
-        #         eb.set_property("name", "task-box-focused")
-        #     else:
-        #         eb.set_property("name", "task-box")
 
         if self.settings["show-icon"]:
             self.pack_start(self.icon, False, False, 6)
@@ -117,9 +79,6 @@ class HyprlandWorkspaces(Gtk.Box):
         if self.settings["angle"] != 0.0:
             lbl.set_angle(self.settings["angle"])
             self.name_label.set_angle(self.settings["angle"])
-
-        self.ws_num2box[num] = eb
-        self.ws_num2lbl[num] = lbl
 
         box.pack_start(lbl, False, False, 6)
 
@@ -180,60 +139,6 @@ class HyprlandWorkspaces(Gtk.Box):
 
         if not loaded_icon and self.icon.get_visible():
             self.icon.hide()
-
-    def find_details(self):
-        tree = self.i3.get_tree()
-        workspaces = self.i3.get_workspaces()
-        ws_num = -1
-        win_name = ""
-        win_id = ""  # app_id if available, else window_class
-        layout = None
-        numbers = []
-
-        for ws in workspaces:
-            numbers.append(str(ws.num))
-            if ws.focused:
-                ws_num = ws.num
-
-        non_empty = []
-        if self.settings["show-name"] or self.settings["show-icon"]:
-            f = self.i3.get_tree().find_focused()
-            if f.type == "con" and f.name and str(f.parent.workspace().num) in self.settings["numbers"]:
-                win_name = f.name[:self.settings["name-length"]]
-
-                if f.app_id:
-                    win_id = f.app_id
-                elif f.window_class:
-                    win_id = f.window_class
-
-            for item in tree.descendants():
-                if item.type == "workspace":
-                    # find non-empty workspaces
-                    if self.settings["mark-content"] or self.settings["hide-empty"]:
-                        tasks_num = 0
-                        for d in item.descendants():
-                            if d.type == "con" and d.name:
-                                tasks_num += 1
-                        if tasks_num > 0:
-                            non_empty.append(item.num)
-
-                    for node in item.floating_nodes:
-                        if str(node.workspace().num) in self.settings["numbers"]:
-                            if node.focused and node.name:
-                                win_name = node.name[:self.settings["name-length"] - 1]
-
-                                if node.app_id:
-                                    win_id = node.app_id
-                                elif node.window_class:
-                                    win_id = node.window_class
-                                layout = "floating"
-
-                            non_empty.append(node.workspace().num)
-
-            if not layout:
-                layout = f.parent.layout
-
-        return ws_num, win_name, win_id, non_empty, layout, numbers
 
     def on_click(self, event_box, event_button, num):
         nwg_panel.common.i3.command("workspace number {}".format(num))
