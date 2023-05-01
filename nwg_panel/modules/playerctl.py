08a135c13244fb57d09c68d70bf0434f22be40fb
MoetaYuko <loli@yuko.moe>
2023-05-01 22:47:31 +0800

    playerctl: Refactor to make use of playerctl api

--- nwg_panel/modules/playerctl.py
+++ nwg_panel/modules/playerctl.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
+from enum import Enum
 import os.path
-
-from gi.repository import GLib
-
-import subprocess
 import threading
-import requests
-
-from nwg_panel.tools import check_key, update_image, player_status, player_metadata, eprint, local_dir
+from urllib.parse import unquote, urlparse
 
 import gi
+gi.require_version('Playerctl', '2.0')
 
-gi.require_version('Gtk', '3.0')
-gi.require_version('Gdk', '3.0')
+from gi.repository import GLib, Gtk
+from gi.repository import Playerctl as Ctl
+import requests
 
-from gi.repository import Gtk, Gdk
-
-from urllib.parse import unquote, urlparse
+from nwg_panel.tools import check_key, eprint, local_dir, update_image
 
 
 class Playerctl(Gtk.EventBox):
+    PlayerOps = Enum('PlayerOps', ['PLAY_PAUSE', 'NEXT', 'PREVIOUS'])
+
     def __init__(self, settings, icons_path=""):
         self.settings = settings
         self.icons_path = icons_path
@@ -36,19 +33,110 @@ class Playerctl(Gtk.EventBox):
         check_key(settings, "angle", 0.0)
         check_key(settings, "button-css-name", "")
 
-        self.status = ""
-        self.retries = 2  # to avoid hiding the module on forward / backward btn when playing from the browser
-
-        self.output_start_idx = 0
-        self.old_metadata = ""
         self.old_cover_url = ""
+        self.old_media_info = ""
+
+        self.player = None
+        self.player_handler_ids = []
 
         self.build_box()
+        self.subscribe()
 
-        self.refresh()
+        # Hide on start if no player presents
+        def hide_self(_):
+            if not self.player:
+                self.hide()
 
-        if settings["interval"] > 0:
-            Gdk.threads_add_timeout_seconds(GLib.PRIORITY_LOW, settings["interval"], self.refresh)
+        self.connect("realize", hide_self)
+
+    def subscribe(self):
+        # Must associate manager with self to increase its reference count
+        self.manager = Ctl.PlayerManager()
+        self.manager.connect('name-appeared', self.on_name_appeared)
+        self.manager.connect('player-vanished', self.on_player_vanished)
+
+        # Manage all players from old to new, so that the newest one comes
+        # first in props.players
+        for name in reversed(self.manager.props.player_names):
+            self.manage_player_by_name(self.manager, name)
+
+        if len(self.manager.props.players) > 0:
+            # Populate the top player only
+            self.init_player(self.manager.props.players[0])
+
+    @staticmethod
+    def manage_player_by_name(manager, name):
+        player = Ctl.Player.new_from_name(name)
+        manager.manage_player(player)
+
+    def on_name_appeared(self, manager, name):
+        self.deinit_player()
+        self.manage_player_by_name(manager, name)
+        self.init_player(manager.props.players[0])
+
+    def on_player_vanished(self, manager, player):
+        # Non-active player vanished, do nothing
+        if self.player and player.props.player_name != self.player.props.player_name:
+            return
+
+        # Active player vanished, populate another one if exists
+        self.deinit_player()
+        if len(manager.props.players) > 0:
+            self.init_player(manager.props.players[0])
+
+    def init_player(self, player):
+        self.player = player
+        self.show()
+
+        # connect signals
+        self.player_handler_ids.append(
+            player.connect('metadata', self.on_metadata))
+        self.player_handler_ids.append(
+            player.connect('playback-status', self.on_playback_status))
+
+        # Manually set the initial state
+        self.on_metadata(player, player.props.metadata)
+
+    def deinit_player(self):
+        if self.player:
+            for handler_id in self.player_handler_ids:
+                self.player.disconnect(handler_id)
+        self.player = None
+        self.player_handler_ids.clear()
+        self.hide()
+
+    def on_playback_status(self, player, status):
+        artist = player.get_artist()
+        title = player.get_title()
+        status_text = None
+
+        if status == Ctl.PlaybackStatus.PLAYING:
+            update_image(self.play_pause_btn.get_image(), "media-playback-pause-symbolic",
+                         self.settings["icon-size"], icons_path=self.icons_path)
+        else:
+            update_image(self.play_pause_btn.get_image(), "media-playback-start-symbolic",
+                         self.settings["icon-size"], icons_path=self.icons_path)
+            if status == Ctl.PlaybackStatus.PAUSED:
+                status_text = "paused"
+            elif status == Ctl.PlaybackStatus.STOPPED:
+                status_text = "stopped"
+
+        # Filter out empty value when building info
+        info = [x for x in (artist, title, status_text) if x]
+        info = " - ".join(info)
+        self.set_media_info(info)
+
+    def on_metadata(self, player, metadata):
+        try:
+            cover_url = metadata["mpris:artUrl"]
+        except KeyError:
+            cover_url = ""
+
+        if cover_url != self.old_cover_url:
+            self.old_cover_url = cover_url
+            self.update_cover_image(cover_url)
+
+        self.on_playback_status(player, player.props.playback_status)
 
     def update_remote_cover(self, url):
         try:
@@ -80,70 +168,6 @@ class Playerctl(Gtk.EventBox):
         if not path:
             update_image(self.cover_img, "music", self.settings["cover-size"], self.icons_path)
 
-    def update_widget(self, status, metadata):
-        text = metadata["text"]
-        self.label.set_tooltip_text(text)
-        if status in ["Playing", "Paused"]:
-            self.retries = 2
-            if not self.get_visible():
-                self.show()
-
-            if not self.status == status:
-                if status == "Playing":
-                    update_image(self.play_pause_btn.get_image(), "media-playback-pause-symbolic",
-                                 self.settings["icon-size"], icons_path=self.icons_path)
-                elif status == "Paused":
-                    update_image(self.play_pause_btn.get_image(), "media-playback-start-symbolic",
-                                 self.settings["icon-size"], icons_path=self.icons_path)
-                    text = "{} - paused".format(text)
-
-            # reset (scrolling) if track changed
-            if metadata != self.old_metadata:
-                self.output_start_idx = 0
-                self.update_cover_image(metadata["url"])
-                self.old_metadata = metadata
-
-            if not self.settings["scroll"]:
-                self.label.set_text(text)
-            else:
-                # scroll track metadata of 1 character once settings["interval"]
-                if len(text) > self.settings["chars"]:
-                    if self.output_start_idx + self.settings["chars"] <= len(text):
-                        self.label.set_text(
-                            text[self.output_start_idx:self.output_start_idx + self.settings["chars"]])
-                        self.output_start_idx += 1
-                    else:
-                        self.label.set_text(text[:self.settings["chars"]])
-                        self.output_start_idx = 0
-                else:
-                    self.label.set_text(text)
-        else:
-            if self.get_visible():
-                if self.retries == 0:
-                    self.hide()
-                else:
-                    self.retries -= 1
-
-        return False
-
-    def get_output(self):
-        metadata = {"text": "", "url": ""}
-        try:
-            status = player_status()
-            if status in ["Playing", "Paused"]:
-                metadata = player_metadata()
-                if not self.settings["scroll"] and len(metadata["text"]) > self.settings["chars"]:
-                    metadata["text"] = metadata["text"][:self.settings["chars"] - 1]
-            GLib.idle_add(self.update_widget, status, metadata)
-        except Exception as e:
-            print(e)
-
-    def refresh(self):
-        thread = threading.Thread(target=self.get_output)
-        thread.daemon = True
-        thread.start()
-        return True
-
     def build_box(self):
         self.box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
         if self.settings["angle"] != 0.0:
@@ -161,7 +185,7 @@ class Playerctl(Gtk.EventBox):
         btn.set_image(img)
         if self.settings["button-css-name"]:
             btn.set_property("name", self.settings["button-css-name"])
-        btn.connect("clicked", self.launch, "playerctl previous")
+        btn.connect("clicked", self.launch, self.PlayerOps.PREVIOUS)
         button_box.pack_start(btn, False, False, 1)
 
         self.play_pause_btn = Gtk.Button()
@@ -170,7 +194,7 @@ class Playerctl(Gtk.EventBox):
         img = Gtk.Image()
         update_image(img, "media-playback-start-symbolic", self.settings["icon-size"], icons_path=self.icons_path)
         self.play_pause_btn.set_image(img)
-        self.play_pause_btn.connect("clicked", self.launch, "playerctl play-pause")
+        self.play_pause_btn.connect("clicked", self.launch, self.PlayerOps.PLAY_PAUSE)
         button_box.pack_start(self.play_pause_btn, False, False, 1)
 
         img = Gtk.Image()
@@ -179,10 +203,12 @@ class Playerctl(Gtk.EventBox):
         btn.set_image(img)
         if self.settings["button-css-name"]:
             btn.set_property("name", self.settings["button-css-name"])
-        btn.connect("clicked", self.launch, "playerctl next")
+        btn.connect("clicked", self.launch, self.PlayerOps.NEXT)
         button_box.pack_start(btn, False, False, 1)
 
-        self.label = Gtk.Label()
+        self.label = AutoScrollLabel(self.settings["scroll"],
+                                     self.settings["chars"],
+                                     self.settings["interval"])
         if self.settings["label-css-name"]:
             self.label.set_property("name", self.settings["label-css-name"])
         self.label.set_angle(self.settings["angle"])
@@ -201,5 +227,67 @@ class Playerctl(Gtk.EventBox):
             self.box.pack_start(self.label, False, False, 2)
             self.box.pack_start(button_box, False, False, 10)
 
-    def launch(self, button, cmd):
-        subprocess.Popen('exec {}'.format(cmd), shell=True)
+    def launch(self, button, op):
+        if not self.player:
+            return
+
+        props = self.player.props
+
+        if op == self.PlayerOps.PLAY_PAUSE:
+            status = props.playback_status
+            if status == Ctl.PlaybackStatus.PLAYING:
+                if not props.can_pause:
+                    return
+            else:
+                if not props.can_play:
+                    return
+            self.player.play_pause()
+
+        elif op == self.PlayerOps.PREVIOUS:
+            if props.can_go_previous:
+                self.player.previous()
+        elif op == self.PlayerOps.NEXT:
+            if props.can_go_next:
+                self.player.next()
+
+    def set_media_info(self, text):
+        if self.old_media_info != text:
+            self.old_media_info = text
+            self.label.set_tooltip_text(text)
+            self.label.set_text(text)
+
+
+class AutoScrollLabel(Gtk.Label):
+    def __init__(self, scroll, chars, interval):
+        super().__init__(self)
+        self.chars = chars
+        self.interval = interval if scroll else 0
+
+        self.output_start_idx = 0
+        self.text = ""
+        self.src = 0
+
+    def set_text(self, text):
+        self.text = text
+        self.output_start_idx = 0
+        super().set_text(text[:self.chars])
+
+        if self.interval == 0 or len(text) <= self.chars:
+            # Disable scroll
+            if self.src > 0:
+                GLib.Source.remove(self.src)
+                self.src = 0
+        else:
+            # Enable scroll
+            if self.src == 0:
+                self.src = GLib.timeout_add_seconds(self.interval,
+                                                    self.scroll_text,
+                                                    priority=GLib.PRIORITY_LOW)
+
+    def scroll_text(self):
+        self.output_start_idx += 1
+        if self.output_start_idx + self.chars > len(self.text):
+            self.output_start_idx = 0
+        super().set_text(
+            self.text[self.output_start_idx:self.output_start_idx + self.chars])
+        return True
