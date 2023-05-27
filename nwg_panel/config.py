b060007289cdccb9a51928d5c48d02ff7b17b8ae
piotr <nwg.piotr@gmail.com>
2023-05-27 02:20:06 +0200

    read settings["workspace-buttons"] from the form

--- nwg_panel/config.py
+++ nwg_panel/config.py
@@ -1319,6 +1319,7 @@ class EditorWrapper(object):
         settings["show-app-icon"] = self.ckb_show_app_icon.get_active()
         settings["show-app-name"] = self.ckb_show_app_name.get_active()
         settings["show-layout"] = self.ckb_show_layout.get_active()
+        settings["workspace-buttons"] = self.workspace_buttons.get_active()
         settings["mark-xwayland"] = self.ckb_mark_xwayland.get_active()
         settings["all-outputs"] = self.ckb_all_outputs.get_active()
 
