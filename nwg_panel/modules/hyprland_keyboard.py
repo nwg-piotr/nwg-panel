b462cf3e0f7e2df5f9a3075cc95f1c0742fb94bc
Jiri Batelka <batys98@gmail.com>
2023-09-01 18:02:00 +0200

    now a button

--- nwg_panel/modules/hyprland_keyboard.py
+++ nwg_panel/modules/hyprland_keyboard.py
@@ -5,24 +5,14 @@ from gi.repository import Gtk, Gdk
 from nwg_panel.tools import check_key, update_image_fallback_desktop, hyprctl
 
 
-class HyprlandKeyboard(Gtk.Box):
+class HyprlandKeyboard(Gtk.Button):
     def __init__(self, settings, devices):
-        Gtk.Box.__init__(self, orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
+        Gtk.Button.__init__(self)
         self.settings = settings
-        self.label = Gtk.Label()
 
-        self.build_box()
         self.refresh(devices)
-
-    def build_box(self):
-        check_key(self.settings, "angle", 0.0)
-        check_key(self.settings, "device", None)
-
-        if self.settings["angle"] != 0.0:
-            self.set_orientation(Gtk.Orientation.VERTICAL)
-            self.num_box.set_orientation(Gtk.Orientation.VERTICAL)
-
-        self.pack_start(self.label, False, False, 0)
+        self.connect("clicked", self.on_click)
+        self.show()
 
     def get_active_keymap(self, devices):
         if self.settings["device"]:
@@ -36,19 +26,8 @@ class HyprlandKeyboard(Gtk.Box):
 
     def refresh(self, devices):
         layout = self.get_active_keymap(devices)
-        self.label.set_text(layout)
+        self.set_label(layout)
         
 
-    def on_click(self, event_box, event_button, num):
-        hyprctl("dispatch switchxkblayout next")
-
-    def on_scroll(self, event_box, event):
-        pass
-
-    def on_enter_notify_event(self, widget, event):
-        widget.set_state_flags(Gtk.StateFlags.DROP_ACTIVE, clear=False)
-        widget.set_state_flags(Gtk.StateFlags.SELECTED, clear=False)
-
-    def on_leave_notify_event(self, widget, event):
-        widget.unset_state_flags(Gtk.StateFlags.DROP_ACTIVE)
-        widget.unset_state_flags(Gtk.StateFlags.SELECTED)
+    def on_click(self, button):
+        hyprctl("switchxkblayout {} next".format(self.settings["device"]))
