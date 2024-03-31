f229cf9bdad93b83150563ead934feda88391d01
piotr <nwg.piotr@gmail.com>
2024-04-01 01:28:26 +0200

    add airplane_mode.py script

---
+++ nwg_panel/executors/airplane_mode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import subprocess
+
+
+def get_output(cmd):
+    try:
+        return subprocess.check_output(cmd, shell=True).decode("utf-8").strip().splitlines()
+    except subprocess.CalledProcessError:
+        return ""
+
+
+o = get_output("rfkill list")
+for line in o:
+    if "Soft blocked: yes" in line or "Hard blocked: yes" in line:
+        print("airplane-mode-symbolic.svg")
+        print("")
+        break
