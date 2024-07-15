8a5923153fedbaa421187160b28410d32d29ed39
piotr <nwg.piotr@gmail.com>
2024-07-16 01:47:27 +0200

    debug

--- nwg_panel/main.py
+++ nwg_panel/main.py
@@ -347,7 +347,6 @@ def instantiate_content(panel, container, content_list, icons_path=""):
         if item == "hyprland-workspaces":
             if his:
                 if "hyprland-workspaces" in panel:
-                    print("activeworkspace:", activeworkspace)
                     workspaces = HyprlandWorkspaces(panel["hyprland-workspaces"], monitors, workspaces, clients,
                                                     activewindow, activeworkspace, icons_path=icons_path)
                     container.pack_start(workspaces, False, False, panel["items-padding"])
--- nwg_panel/modules/hyprland_workspaces.py
+++ nwg_panel/modules/hyprland_workspaces.py
@@ -7,6 +7,7 @@ from nwg_panel.tools import check_key, update_image_fallback_desktop, hyprctl
 
 class HyprlandWorkspaces(Gtk.Box):
     def __init__(self, settings, monitors, workspaces, clients, activewindow, activeworkspace, icons_path):
+        print("HyprlandWorkspaces activeworkspace:", activeworkspace)
         Gtk.Box.__init__(self, orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
         self.settings = settings
         self.num_box = Gtk.Box.new(Gtk.Orientation.HORIZONTAL, 0)
