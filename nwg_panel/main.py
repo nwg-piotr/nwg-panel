503b66b0774983a684b20e4c5415d65c1720f875
piotr <nwg.piotr@gmail.com>
2024-07-16 01:40:47 +0200

    debug

--- nwg_panel/main.py
+++ nwg_panel/main.py
@@ -348,8 +348,7 @@ def instantiate_content(panel, container, content_list, icons_path=""):
             if his:
                 if "hyprland-workspaces" in panel:
                     workspaces = HyprlandWorkspaces(panel["hyprland-workspaces"], monitors, workspaces, clients,
-                                                    activewindow, activeworkspace, icons_path=icons_path)
-                    print("workspaces", workspaces)
+                                                    activewindow, {}, icons_path=icons_path)
                     container.pack_start(workspaces, False, False, panel["items-padding"])
                     common.h_workspaces_list.append(workspaces)
                     print("Successfully instantiated hyprland-workspaces")
