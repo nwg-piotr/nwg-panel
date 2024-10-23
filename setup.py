970eafced68eeab46fc4cdeb7b1bb5bf8b60f9aa
piotr <nwg.piotr@gmail.com>
2024-10-24 00:11:11 +0200

    bump to 0.9.48

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(f_name):
 
 setup(
     name='nwg-panel',
-    version='0.9.47',
+    version='0.9.48',
     description='GTK3-based panel for sway and Hyprland Wayland compositors',
     packages=find_packages(),
     include_package_data=True,
