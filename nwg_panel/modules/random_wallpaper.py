2d3d20fd1703dc8364ec398821968db1a3b47624
piotr <nwg.piotr@gmail.com>
2024-10-29 18:12:10 +0100

    restore killing swaybg!

--- nwg_panel/modules/random_wallpaper.py
+++ nwg_panel/modules/random_wallpaper.py
@@ -37,7 +37,7 @@ class RandomWallpaper(Gtk.Button):
             "save-path": "",
             "local-path": "",
             "icon-size": 16,
-            "interval": 1,
+            "interval": 0,
             "refresh-on-startup": True,
         }
         for key in defaults:
