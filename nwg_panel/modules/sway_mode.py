3fe70be5ac052b6f5d9581afa5870078c6ce6cdb
piotr <nwg.piotr@gmail.com>
2024-12-19 01:16:36 +0100

    check mode on startup

--- nwg_panel/modules/sway_mode.py
+++ nwg_panel/modules/sway_mode.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import json
+import subprocess
+
 from gi.repository import Gtk, GLib
 from i3ipc import Event
 
@@ -12,6 +15,7 @@ class SwayMode(Gtk.Box):
         self.settings = settings
         self.i3 = i3
         self.label = Gtk.Label.new("default")
+        self.mode = "default"
 
         icon = Gtk.Image()
 
@@ -41,15 +45,33 @@ class SwayMode(Gtk.Box):
 
         self.pack_start(self.label, False, False, 2)
 
-        if not self.settings["show-default"]:
+        self.check_initial_mode()
+
+        if self.mode == "default" and not self.settings["show-default"]:
             GLib.idle_add(self.hide, priority=GLib.PRIORITY_HIGH)
 
         self.i3.on(Event.MODE, self.on_i3ipc_event)
 
+    def check_initial_mode(self):
+        # On panel startup we may already be in some mode other than default,
+        # but we only check it on i3ipc event.
+        # As i3ipc seemingly does not implement the `get_binding_state` function,
+        # for initial check we need to parse `swaymsg -t get_binding_state` output.
+        self.mode = "default"
+        o = subprocess.check_output("swaymsg -t get_binding_state".split()).decode("utf-8")
+        try:
+            self.mode = json.loads(o)["name"]
+        except KeyError:
+            pass
+        self.set_visibility()
+
     def on_i3ipc_event(self, i3conn, event):
-        mode = event.change
-        self.label.set_text(mode)
-        if mode != "default" or self.settings["show-default"]:
+        self.mode = event.change
+        self.set_visibility()
+
+    def set_visibility(self):
+        self.label.set_text(self.mode)
+        if self.mode != "default" or self.settings["show-default"]:
             self.show_all()
         else:
             self.hide()
