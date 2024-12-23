9468a79c16c41772e4f7d5f20ffc05686c894438
Pol Rivero <65060696+pol-rivero@users.noreply.github.com>
2024-12-23 17:22:26 +0100

    Implement launch filters
    "Run if outputs exist": Comma-separated list of outputs which must all be connected in order for this panel to be launched
    
    "Run if outputs absent": Comma-separated list of outputs which must all be disconnected in order for this panel to be launched

--- nwg_panel/main.py
+++ nwg_panel/main.py
@@ -589,6 +589,8 @@ def main():
     for panel in panels:
         check_key(panel, "output", "")
         check_key(panel, "monitor", "")
+        check_key(panel, "run-if-output-exist", [])
+        check_key(panel, "run-if-output-absent", [])
         if panel["monitor"]:
             try:
                 panel["output"] = common.mon_desc2output_name[panel["monitor"]]
@@ -604,9 +606,17 @@ def main():
                 clones.append(clone)
 
             to_append = to_append + clones
+        else:
+            for output in panel["run-if-output-exist"]:
+                if output not in common.outputs:
+                    to_remove.append(panel)
+            for output in panel["run-if-output-absent"]:
+                if output in common.outputs:
+                    to_remove.append(panel)
 
     for item in to_remove:
-        panels.remove(item)
+        if item in panels:
+            panels.remove(item)
 
     panels = panels + to_append
 
