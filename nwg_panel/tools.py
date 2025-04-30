a13e1570f1b23823d0c7d28e55b958ede9dec1db
piotr <nwg.piotr@gmail.com>
2025-04-30 03:04:44 +0200

    cmd_through_compositor/Niri: do not enclose parts in quotation marks

--- nwg_panel/tools.py
+++ nwg_panel/tools.py
@@ -926,12 +926,7 @@ def cmd_through_compositor(cmd):
         elif os.getenv("HYPRLAND_INSTANCE_SIGNATURE"):
             cmd = f'hyprctl dispatch exec "{cmd}"'
         elif os.getenv("NIRI_SOCKET"):
-            parts = cmd.split()
-            quoted = []
-            for part in parts:
-                quoted.append(f'"{part}"')
-            _string = " ".join(quoted)
-            cmd = f'niri msg action spawn -- {_string}'
+            cmd = f'niri msg action spawn -- {cmd}'
     return cmd
 
 
